4e11a813bc35172c4c908ab8aaa7aebbed937370
snowme34 <snowme34@users.noreply.github.com>
2019-07-21 15:39:28 -0700

    remove magic hard-coded bug

--- src/lib/loadSW.js
+++ src/lib/loadSW.js
@@ -1,7 +1,6 @@
 // registers SW
 export default function registerSW() {
 	if ('serviceWorker' in navigator) {
-		const prefix = (window.location.host.indexOf('localhost') < 0) ? '/work' : '';
 		navigator.serviceWorker.register(prefix + '/sw.js', { scope: prefix + '/' })
 			.then(function (registration) {
 				console.log('Service Worker Registered');
