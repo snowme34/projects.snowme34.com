c1f993a4d7cfc838f9d8d2e8e73148fb7848e109
snowme34 <snowme34@users.noreply.github.com>
2019-07-21 22:34:50 -0700

    pay for aggressive upgrading

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -16,12 +16,10 @@ module.exports = {
 			}
 		}, {
 			test: /\.css$/,
-			use:  ExtractTextPlugin.extract({
-				use: [{
-					loader: 'css-loader',
-					options: { importLoaders: 1, modules: true, localIdentName: '[local]-[hash:base64:5]' },
-				}],
-			}),
+			use: [{
+				loader: 'css-loader',
+				options: { importLoaders: 1, modules: true, localIdentName: '[local]-[hash:base64:5]' },
+			}],
 		}]
 	},
 	resolve: {
@@ -38,8 +36,15 @@ module.exports = {
 	},
 	node: { Buffer: false },  // https://github.com/btmills/geopattern/issues/32
 	plugins: [
-		// extract text
-		new ExtractTextPlugin("styles.css"),
+		new ExtractTextPlugin('styles.css'),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    })
 		// vendor: https://webpack.github.io/docs/code-splitting.html#split-app-and-vendor-code
 		// new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js'})
 		// no need for vendor optimization as it is very small
